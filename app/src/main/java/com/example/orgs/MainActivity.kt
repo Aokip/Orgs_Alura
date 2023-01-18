84b2d61c3aca4125c4353616a398b80aa59887d2
Patrick Aoki <pbelo22@gmail.com>
2023-01-18 20:46:15 -0300

    "Configuração lista de itens (Adapter)"

--- app/src/main/java/com/example/orgs/MainActivity.kt
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.orgs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.example.orgs.adapter.ListaDeDadosAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rv = findViewById<RecyclerView>(R.id.rv)
+        /* configuração do RecyclerView incluindo o Adapter */
+        rv.adapter = ListaDeDadosAdapter()
+        rv.layoutManager = LinearLayout(this)
     }
 }
---
+++ app/src/main/java/com/example/orgs/adapter/ListaDeDadosAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.orgs.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ListaDeDadosAdapter : RecyclerView.Adapter<ListaDeDadosAdapter.ViewHolder>() {
+    class ViewHolder (Listview : View) : RecyclerView.ViewHolder(Listview){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
