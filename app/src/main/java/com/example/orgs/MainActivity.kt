d978347e0ba7ca76354f7bb7c85446c7dcaccc36
Patrick Aoki <pbelo22@gmail.com>
2023-01-18 21:06:44 -0300

    "Incluido o Recycler e adapter na Main para a exibição em tela"

--- app/src/main/java/com/example/orgs/MainActivity.kt
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.orgs
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.orgs.adapter.ListaDeDadosAdapter
+import com.example.orgs.adapter.lista.Cadastro
+import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +14,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rv = findViewById<RecyclerView>(R.id.rv)
         /* configuração do RecyclerView incluindo o Adapter */
-        rv.adapter = ListaDeDadosAdapter()
-        rv.layoutManager = LinearLayout(this)
+        val lista = listOf<Cadastro>(Cadastro(nome="oi", email= "oi", phone= BigDecimal("1254")))
+        rv.adapter = ListaDeDadosAdapter(lista)
+        rv.layoutManager = LinearLayoutManager(this)
     }
 }
