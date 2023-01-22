1a976f9dd8036ea80d1fff99ae1350cc35355f3d
Patrick Aoki <pbelo22@gmail.com>
2023-01-21 21:33:20 -0300

    "Ajustando em CampanionOBJ"

--- app/src/main/java/com/example/orgs/DAO.kt
+++ app/src/main/java/com/example/orgs/DAO.kt
@@ -3,12 +3,18 @@ package com.example.orgs
 import com.example.orgs.adapter.lista.Cadastro
 
 class DAO {
-    private val listadeCadastro = mutableListOf<Cadastro>()
     fun adicionar (cadastro : Cadastro){
         listadeCadastro.add(cadastro)
 
     }
-    fun bucatodos () : List<Cadastro>{
-      return listadeCadastro.toList()
+
+
+        fun buscatodos() : List<Cadastro>{
+          return listadeCadastro.toList()
+
+    }
+
+    companion object {
+        private val listadeCadastro = mutableListOf<Cadastro>()
     }
 }
