862879b0e01339fae5077d939909d54ad023a856
Patrick Aoki <pbelo22@gmail.com>
2023-01-24 21:50:50 -0300

    "Criado função da ação de salvar o cadastro "AdicionaCadastro" Criado a função para abrir o dialogo "AbreDialog" utilizando a imageview"

--- app/src/main/java/com/example/orgs/CadastroUsuario.kt
+++ app/src/main/java/com/example/orgs/CadastroUsuario.kt
@@ -1,8 +1,11 @@
 package com.example.orgs
 
+import android.app.AlertDialog
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import com.example.orgs.adapter.lista.Cadastro
 import java.math.BigDecimal
@@ -12,8 +15,27 @@ class CadastroUsuario : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_dados)
-
+        val img = findViewById<ImageView>(R.id.img_cadastro_usuario)
         val bt = findViewById<Button>(R.id.botao)
+        AdicionaCadastro(bt)
+        AbreDialog(img)
+    }
+
+
+
+
+    private fun AbreDialog(img: ImageView) {
+        img.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setView(R.layout.layout_dialog)
+                .setPositiveButton("Confirmar") { _, _ -> }
+                .setNegativeButton("Cancelar") { _, _ -> }
+                .show()
+        }
+    }
+
+
+    private fun AdicionaCadastro(bt: Button) {
         bt.setOnClickListener {
             val nome = findViewById<TextView>(R.id.cadastro_nome)
             val email = findViewById<TextView>(R.id.cadastro_email)
@@ -21,13 +43,12 @@ class CadastroUsuario : AppCompatActivity() {
             val recuperanome = nome.text.toString()
             val recuperaemail = email.text.toString()
             val recuperaphone = phone.text.toString()
-            val dadosphone = if (recuperaphone.isBlank()){
+            val dadosphone = if (recuperaphone.isBlank()) {
                 BigDecimal.ZERO
-            }
-            else{
+            } else {
                 recuperaphone.toBigDecimal()
             }
-            val cadastro = Cadastro(recuperanome,recuperaemail, dadosphone)
+            val cadastro = Cadastro(recuperanome, recuperaemail, dadosphone)
 
             dao.adicionar(cadastro)
             finish()
