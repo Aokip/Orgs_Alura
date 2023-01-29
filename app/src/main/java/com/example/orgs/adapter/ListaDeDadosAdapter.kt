37c5f41d95ad59b313cf004e73291800561e0169
Patrick Aoki <pbelo22@gmail.com>
2023-01-28 21:41:04 -0300

    "Adicionado o campo de imagem e alocando o load"

--- app/src/main/java/com/example/orgs/adapter/ListaDeDadosAdapter.kt
+++ app/src/main/java/com/example/orgs/adapter/ListaDeDadosAdapter.kt
@@ -3,8 +3,10 @@ package com.example.orgs.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
 import com.example.orgs.R
 import com.example.orgs.adapter.lista.Cadastro
 
@@ -17,10 +19,12 @@ class ListaDeDadosAdapter(
             val nome = itemView.findViewById<TextView>(R.id.edt_nome_dados)
             val email = itemView.findViewById<TextView>(R.id.edt_email_dados)
             val phone = itemView.findViewById<TextView>(R.id.edt_phone_dados)
+            val img = itemView.findViewById<ImageView>(R.id.img_rv_dados)
 
             nome.text = posicaoLista.nome
             email.text = posicaoLista.email
             phone.text = posicaoLista.phone.toString()
+            img.load(posicaoLista.url)
 
         }
 
