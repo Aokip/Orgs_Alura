740904016d0bb75d8932c99c90aadb9b13155f17
Patrick Aoki <pbelo22@gmail.com>
2023-01-28 21:37:53 -0300

    "Incluido a variavel "URL" para utilizar o COIL na imageView do Recyclerview"

--- app/src/main/java/com/example/orgs/adapter/lista/Cadastro.kt
+++ app/src/main/java/com/example/orgs/adapter/lista/Cadastro.kt
@@ -5,5 +5,6 @@ import java.math.BigDecimal
 data class Cadastro (
     val nome : String,
     val email: String,
-    val phone : BigDecimal
+    val phone : BigDecimal,
+    var url : String? = null,
         )
