aebc6ffae457f8470c7ebc4ff9664557b9176f67
Patrick Aoki <pbelo22@gmail.com>
2023-02-05 17:45:03 -0300

    "retirado importações não utilizadas"

--- app/src/main/java/com/example/orgs/extenx/TentaCarregarImagem.kt
+++ app/src/main/java/com/example/orgs/extenx/TentaCarregarImagem.kt
@@ -3,6 +3,7 @@ package com.example.orgs.extenx
 import android.widget.ImageView
 import coil.load
 import com.example.orgs.R
+
 /* Cadastro de função separada dos xml visto que não é dependente do layout
 função carrega o load do coil e utiliza o fallback e o error para preencher caso os dados não
 forem fornecidos ou forem invalidos
