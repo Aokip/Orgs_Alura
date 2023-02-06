716f6996183430ab264548e57a2982b5e7da9eb1
Patrick Aoki <pbelo22@gmail.com>
2023-02-06 20:57:21 -0300

    "Criado uma nova act para apresentar todos os dados do rv em uma nova tela"

---
+++ app/src/main/java/com/example/orgs/Dadositem.kt
@@ -0,0 +1,12 @@
+package com.example.orgs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Dadositem : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dados_item)
+
+    }
+}
