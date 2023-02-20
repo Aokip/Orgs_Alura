b092df62d01e93fae356044c900ccf48b5602ea5
Patrick Aoki <pbelo22@gmail.com>
2023-02-20 18:15:04 -0300

    "Ajusta para recuperar obj ao clicar no rv"

--- app/src/main/java/com/example/orgs/adapter/ListaDeDadosAdapter.kt
+++ app/src/main/java/com/example/orgs/adapter/ListaDeDadosAdapter.kt
@@ -1,24 +1,43 @@
 package com.example.orgs.adapter
-
-import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import coil.ImageLoader
-import coil.decode.ImageDecoderDecoder
 import coil.load
 import com.example.orgs.R
 import com.example.orgs.adapter.lista.Cadastro
 
 class ListaDeDadosAdapter(
-    private val itens: List<Cadastro>
+    private val itens: List<Cadastro>,
+    /*
+    criado a variavel quandoclicanoitem como um tipo função, para utilizar expressão lambda em suas
+    chamadas, desta forma ao clicar em um item do RV teremos o objeto do Cadastro.
+     */
+    var quandoclicanoItem: (produto: Cadastro) -> Unit = {}
 ) : RecyclerView.Adapter<ListaDeDadosAdapter.ViewHolder>() {
-    class ViewHolder(Listview: View) : RecyclerView.ViewHolder(Listview) {
+    inner class ViewHolder(Listview: View) : RecyclerView.ViewHolder(Listview) {
+        /*
+        cria uma variavel para ser inicializada depois
+         */
+        private lateinit var produto: Cadastro
 
-        fun bindview(posicaoLista: Cadastro) {
+        /*
+        inicia e faz a checagem se o produto foi inicializado
+         */
+        init {
+            itemView.setOnClickListener {
+                if (::produto.isInitialized)
+                    quandoclicanoItem(produto)
+            }
+        }
+
+        fun bindview(produto: Cadastro) {
+            /*
+            informa que o produto enviado é o do Cadastro
+             */
+            this.produto
             /* Ligação entre a lista "Cadastro" com os edt do "layout_dados" */
             val nome = itemView.findViewById<TextView>(R.id.edt_nome_dados)
             val email = itemView.findViewById<TextView>(R.id.edt_email_dados)
@@ -26,11 +45,10 @@ class ListaDeDadosAdapter(
             val img = itemView.findViewById<ImageView>(R.id.img_rv_dados)
 
 
-            nome.text = posicaoLista.nome
-            email.text = posicaoLista.email
-            phone.text = posicaoLista.phone.toString()
-            img.load(posicaoLista.url)
-
+            nome.text = produto.nome
+            email.text = produto.email
+            phone.text = produto.phone.toString()
+            img.load(produto.url)
 
 
         }
