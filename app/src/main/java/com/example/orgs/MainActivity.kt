6e285baca75c9db3124fd7aea5c31620682199f2
Patrick Aoki <pbelo22@gmail.com>
2023-02-26 15:35:09 -0300

    "Ajustando código"

--- app/src/main/java/com/example/orgs/MainActivity.kt
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -8,14 +8,21 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.orgs.adapter.ListaDeDadosAdapter
 import com.example.orgs.adapter.lista.Cadastro
+import com.example.orgs.databinding.ActivityMainBinding
+import com.example.orgs.databinding.LayoutDadosBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     private val dao = DAO()
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
 
     }
@@ -26,7 +33,13 @@ class MainActivity : AppCompatActivity() {
         val rv = findViewById<RecyclerView>(R.id.rv)
         rv.adapter = ListaDeDadosAdapter(itens = dao.buscatodos())
         rv.layoutManager = LinearLayoutManager(this)
-        val fltAct = findViewById<FloatingActionButton>(R.id.floating_act)
+        AbreCadastro()
+
+
+    }
+
+    private fun AbreCadastro() {
+        val fltAct = binding.floatingAct
         fltAct.setOnClickListener {
             val intente = Intent(this, CadastroUsuario::class.java)
             startActivity(intente)
