6d57979aef8afde44e276fa7a2a2f6df7c04090d
Patrick Aoki <pbelo22@gmail.com>
2023-03-02 21:10:00 -0300

    "Implementando Room no projeto, criado o database "

--- app/src/main/java/com/example/orgs/database/ProdutoDaoRom/AppDataBase.kt
+++ app/src/main/java/com/example/orgs/database/ProdutoDaoRom/AppDataBase.kt
@@ -4,5 +4,8 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 @Database(entities = [ProdutoDaoRom::class], version = 1)
 abstract class AppDataBase: RoomDatabase() {
+    /*
+    Cria uma função abstrata que faz a extensão da classe ProdutoDAOROM
+     */
     abstract fun databaseProduto() : ProdutoDaoRom
 }
