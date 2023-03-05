d2f2f89f92407e01d19f044501755cd2beda55a9
Patrick Aoki <pbelo22@gmail.com>
2023-03-05 15:21:09 -0300

    "Implementado a classe Conversores - > Converter o BigDecimal para Double ao salvar no Room"

---
+++ app/src/main/java/com/example/orgs/database/ProdutoDaoRom/Conversores/Conversores.kt
@@ -0,0 +1,31 @@
+package com.example.orgs.database.ProdutoDaoRom.Conversores
+
+import androidx.room.TypeConverter
+import java.math.BigDecimal
+
+class Conversores {
+    /*
+    Converte o valor do bd de Double para BigDecimal ao ser chamada/apresentada no APP
+    Retorno se o valor for diferente de null, recebe em BigDecimal o valor informado no BD
+    Caso o valor for nulo " ?: " -> Converte a função em Zero, para não "quebrar o APP"
+     */
+    @TypeConverter
+    fun deDouble(valor: Double?): BigDecimal {
+        return valor?.let {
+            BigDecimal(valor.toString())
+        } ?: BigDecimal.ZERO
+    }
+
+    /*
+    Converte o valor informado no APP que está em BigDecimal para Double visto que o sqlite aceita alguns
+    tipos de valores, desta forma converte o BigDecimal para Double para ser salvo no BD.
+    O Double é um tipo que aceita o nulo então não precisa realizar um tratamento caso o valor for nulo, igual
+    a função "deDouble".
+     */
+    fun deBigDecimal(valor: BigDecimal?): Double? {
+        return valor?.let {
+            valor.toDouble()
+        }
+
+    }
+}
